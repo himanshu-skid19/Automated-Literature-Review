245689e7b471a69a3982c4130488f2a1564e97fe
himanshu-skid19 <114365148+himanshu-skid19@users.noreply.github.com>
2024-04-19 23:38:54 +0530

    Update build_vector_store.py

--- build_vector_store.py
+++ build_vector_store.py
@@ -2,36 +2,26 @@ from imports import *
 from keys import *
 from run_llava import *
 from read_docs import *
+import shutil
 
-# def build_vector_store():
-#     openai_mm_llm = OpenAIMultiModal(
-#         model="gpt-4-vision-preview", api_key=OPENAI_API_KEY, max_new_tokens=300
-#     )
+def cleanup():
+    # Delete all PDF files in the pdf_content directory
+    pdf_content_dir = "pdf_content"
+    if os.path.exists(pdf_content_dir):
+        shutil.rmtree(pdf_content_dir)
+    
+    if os.path.exists("pdf_content.pdf"):
+        os.remove("pdf_content.pdf")
+    os.makedirs(pdf_content_dir, exist_ok=True)  # Recreate the directory if it doesn't exist
 
-#     client = qdrant_client.QdrantClient(path="qdrant_mm_db")
+    # Remove the existing vector store directory
+    if os.path.exists("storage"):
+        shutil.rmtree("storage")
 
-#     text_store = QdrantVectorStore(
-#         client=client, collection_name="text_collection"
-#     )
-#     image_store = QdrantVectorStore(
-#         client=client, collection_name="image_collection"
-#     )
-#     storage_context = StorageContext.from_defaults(
-#         vector_store=text_store, image_store=image_store
-#     )
+# Perform cleanup tasks before running the application
+cleanup()
 
-#     # Create the MultiModal index
-#     documents = SimpleDirectoryReader("pdf_content").load_data()
-#     index = MultiModalVectorStoreIndex.from_documents(
-#         documents,
-#         api_key=OPENAI_API_KEY,
-#         storage_context=storage_context,
-#     )
 
-#     # Save it
-#     index.storage_context.persist(persist_dir="./storage")
-
-#     return index
 
 def build_vector_store(text_vector):
     
